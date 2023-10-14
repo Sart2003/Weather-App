2e068d070cf7a303cd3b0f70a8588f4a15069736
Sarthak Saxena <128723723+Sart2003@users.noreply.github.com>
2023-10-14 16:55:52 +0530

    Update app.js
    port change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import path from "path";
 import axios from "axios";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT|| 5000;
 const APIKEY = `82e981428a423f11b34c8087d6b5f90c`;
 const __dirname = path.resolve();
 
